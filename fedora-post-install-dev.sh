fd04a1ff410202d00c782e9a3f326e2f510e44c0
Pierre <pdureau@users.noreply.github.com>
2018-04-30 11:17:50 +0200

    Add Marker md editor

--- fedora-post-install-dev.sh
+++ fedora-post-install-dev.sh
@@ -1,15 +1,16 @@
-# Some generic dev stuff
+# Tooling
 sudo dnf install vim
 sudo flatpak install flathub org.gnome.gitg
 sudo dnf copr enable heikoada/nautilus-git
 sudo dnf install nautilus-git 
+sudo flatpak install flathub com.github.fabiocolacio.marker
 
-# Development stuff - Flatpak
+# Development - Flatpak
 sudo dnf install flatpak-builder
 sudo flatpak install gnome org.gnome.Sdk
 sudo flatpak install kde org.kde.Sdk
 
-# Development stuff - Web
+# Development - Web
 sudo dnf install docker docker-compose
 sudo firewall-cmd --permanent --zone=trusted --add-interface=docker0
 sudo firewall-cmd --permanent --zone=trusted --add-port=4243/tcp
