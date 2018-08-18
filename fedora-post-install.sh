ad8c8f2007d8f217b497421b800e49281a9bd411
Pierre <pdureau@users.noreply.github.com>
2018-08-18 14:05:08 +0200

    Add winepack repo

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -54,6 +54,7 @@ sudo dnf install fuse-sshfs
 
 # Apps sources
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists winepak https://dl.winepak.org/repo/winepak.flatpakrepo
 
 # *************
 # * APPS
