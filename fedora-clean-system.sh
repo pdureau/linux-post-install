170825ea25c828ba588e8904e07a7a31c3d9b431
Pierre <pdureau@users.noreply.github.com>
2019-03-17 17:48:34 +0100

    Add docker prune

--- fedora-clean-system.sh
+++ fedora-clean-system.sh
@@ -3,6 +3,7 @@ sudo flatpak uninstall --unused
 sudo journalctl --vacuum-size=1M
 sudo rm -rf /var/cache/PackageKit/
 sudo dnf clean all --enablerepo=\*
+sudo docker system prune
 
 # Clean /usr
 sudo dnf autoremove
